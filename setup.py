b11bea734cd474ce0dcd4c80e48845aa98846734
dr_forse <george.lifeslice@gmail.com>
2020-04-05 01:02:13 +0100

    add install_requires

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/drforse/dic_academic_api",
+    install_requires=['requests'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
